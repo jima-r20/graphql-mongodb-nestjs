53ada52e511c5cc5af65782a47ff77c6d882c72d
ryuji <ryu.seven.h7420@gmail.com>
2020-10-17 21:47:05 +0900

    [section13-128] idによる特定のStudent取得機能実装(getStudent)

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -12,6 +12,10 @@ export class StudentService {
     private studentRepository: Repository<Student>,
   ) {}
 
+  async getStudent(id: string): Promise<Student> {
+    return this.studentRepository.findOne({ id });
+  }
+
   async getStudents(): Promise<Student[]> {
     return this.studentRepository.find();
   }
